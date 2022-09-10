76a618ecac52042894bb53cfcaa74e633ca7d2e7
Gerardo Hernandez <hernandez.gerardo@bcgplatinion.com>
2022-09-10 15:53:52 -0400

    creacion de componente tradicional

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-dom"
+import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
 
 import logo from '../logo.svg';
 
@@ -10,7 +11,7 @@ export const Navigation = () => {
                     <img src={logo} alt="React Logo" />
                     <ul>
                         <li>
-                            <NavLink to="/home" className={({ isActive }) => isActive ? 'nav-active' : undefined}>Home</NavLink>
+                            <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : undefined}>Shopping</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : undefined}>About</NavLink>
@@ -24,9 +25,9 @@ export const Navigation = () => {
                 <Routes>
                     <Route path="about" element={<h1>About Page</h1>} />
                     <Route path="users" element={<h1>Users Page</h1>} />
-                    <Route path="home" element={<h1>Home Page</h1>} />
+                    <Route path="/" element={ <ShoppingPage /> } />
 
-                    <Route path="/*" element={<Navigate to="/home" replace />} />
+                    <Route path="/*" element={<Navigate to="/" replace />} />
                 </Routes>
             </div>
         </BrowserRouter>
