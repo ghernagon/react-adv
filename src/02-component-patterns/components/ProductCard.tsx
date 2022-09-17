fb75d846bd996c7c841be87fa1d757969a5b8513
Gerardo Hernandez <hernandez.gerardo@bcgplatinion.com>
2022-09-16 22:45:49 -0300

    component extensible styles pattern

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,20 +1,28 @@
-import { createContext } from 'react';
-import { useProduct } from '../hooks/useProduct';
-import { ProductCardProps, ProductContextProps } from '../interfaces/products.interfaces';
-import styles from '../styles/styles.module.css';
-
+import { createContext, ReactElement } from "react";
+import { useProduct } from "../hooks/useProduct";
+import {
+  Product,
+  ProductContextProps,
+} from "../interfaces/products.interfaces";
+import styles from "../styles/styles.module.css";
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ children, product }: ProductCardProps) => {
-    const { counter, increaseBy } = useProduct();
-    return (
-        <Provider value={{ counter, increaseBy, product }}>
-            <div className={styles.productCard}>
-                { children }
-            </div>
-        </Provider>
-    )
+export interface Props {
+  product: Product;
+  children?: ReactElement | ReactElement[];
+  className?: string;
+  style?: React.CSSProperties;
 }
 
+export const ProductCard = ({ children, product, className, style }: Props) => {
+  const { counter, increaseBy } = useProduct();
+  return (
+    <Provider value={{ counter, increaseBy, product }}>
+      <div className={`${styles.productCard} ${className}`} style={style}>
+        {children}
+      </div>
+    </Provider>
+  );
+};
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,39 +1,56 @@
-import { ProductCard, ProductImage, ProductTitle, ProductButtons } from "../components"
-
+import {
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+  ProductButtons,
+} from "../components";
+import "../styles/custon-styles.css";
 
 const product = {
-  id: '1',
-  title: 'Coffee Mug - Card',
-  img: './coffee-mug.png'
-}
+  id: "1",
+  title: "Coffee Mug - Card",
+  img: "./coffee-mug.png",
+};
 
 export const ShoppingPage = () => {
   return (
     <div>
       <h1>Shopping Page</h1>
       <hr />
-      <div style={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-      }}>
-
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
         {/* Compound Component Pattern */}
         {/* Permite crear componentes hijos y establecer relacion directa entre ellos */}
         {/* Las propiedades internas son heredadas desde el padre y se pueden sobreescribir en cada componente */}
-        
-        <ProductCard product={product} >
-          <ProductCard.Image />
-          <ProductCard.Title />
-          <ProductCard.Buttons />
+
+        {/* Extensible Styles Pattern */}
+        {/* Permite usar className y Styles solo o en conjunto */}
+
+        <ProductCard product={product} className="bg-dark text-white">
+          <ProductCard.Image className="custom-image" />
+          <ProductCard.Title className="bold-text" />
+          <ProductCard.Buttons className="custom-buttons" />
         </ProductCard>
 
-        <ProductCard product={product} >
-          <ProductImage />
-          <ProductTitle title={'Cafe'} />
-          <ProductButtons />
+        <ProductCard product={product} className="bg-dark text-white">
+          <ProductImage className="custom-image" />
+          <ProductTitle title={"Cafe"} className="bold-text" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard>
+
+        <ProductCard product={product} style={{ background: "#61DAFB" }}>
+          <ProductImage
+            style={{ boxShadow: "10px 10px 10px rgba(0,0,0,0.2)" }}
+          />
+          <ProductTitle title={"Cafe"} style={{ fontWeight: "bold" }} />
+          <ProductButtons style={{ display: "flex", justifyContent: "end" }} />
         </ProductCard>
       </div>
     </div>
-  )
-}
+  );
+};
